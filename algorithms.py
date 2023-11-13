a78f574e3ee3239ff2ea27b68e8444da29833afc
XDMickeyYau <35620428+XDMickeyYau@users.noreply.github.com>
2023-11-13 15:47:57 -0500

    new naive algo

--- algorithms.py
+++ algorithms.py
@@ -68,7 +68,39 @@ def naive_ver2(mapEntity,  generalData):
                 LK.f3100Count: 1,
             }
     return solution
+
+def naive_ver3(mapEntity,  generalData):
+    solution = {LK.locations: {}}
+
+    for key in mapEntity[LK.locations]:
+        location = mapEntity[LK.locations][key]
+        name = location[LK.locationName]
+
+        salesVolume = location[LK.salesVolume]
+        locationType = location[LK.locationType]
+        footfall = location[LK.footfall]
+
+        min_sale = min(salesVolume,generalData[GK.f3100Data][GK.refillCapacityPerWeek])
+        revenue = min_sale * generalData[GK.refillUnitData][GK.profitPerUnit]
+        earnings = revenue - generalData[GK.f3100Data][GK.leasingCostPerWeek]
         
+        if locationType in ['Grocery-store-large']:
+            solution[LK.locations][name] = {
+                LK.f9100Count: 1,
+                LK.f3100Count: 0,
+            }
+        elif locationType in ['Grocery-store']:
+            solution[LK.locations][name] = {
+                LK.f9100Count: 0,
+                LK.f3100Count: 1,
+            }
+        elif earnings>0 or footfall>0: # salesVolume > generalData[GK.f3100Data][GK.refillCapacityPerWeek]:
+            solution[LK.locations][name] = {
+                LK.f9100Count: 0,
+                LK.f3100Count: 1,
+            }
+    return solution
+     
     
 def algo(name, mapEntity,  generalData):
     return eval(name)(mapEntity,  generalData)
