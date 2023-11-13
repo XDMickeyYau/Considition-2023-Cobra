ddbed0a2795b324a4498902fcf79412636a488b7
XDMickeyYau <35620428+XDMickeyYau@users.noreply.github.com>
2023-11-13 15:49:04 -0500

    add comment in eval output

--- eval.py
+++ eval.py
@@ -16,7 +16,8 @@ apiKey = os.environ["apiKey"]
 
 mapNames = [MN.goteborg, MN.uppsala, MN.vasteras, MN.linkoping]
 results = []
-func_name = 'naive_ver2'
+func_name = 'naive_ver3'
+comment = "naive_ver3"
 
 for mapName in mapNames:
     ##Get map data from Considition endpoint
@@ -33,6 +34,7 @@ for mapName in mapNames:
 
         # Score solution locally
         score = calculateScore(mapName, solution, mapEntity, generalData)
+        print(mapName, score[SK.gameScore])
 
         result = pd.Series(score[SK.gameScore]).to_frame().T
         result.insert(0, 'mapName', mapName)
@@ -42,6 +44,7 @@ for mapName in mapNames:
 results = pd.concat(results,axis=0)
 results.insert(0, 'func_name', func_name)
 results.insert(0, 'timestamp', pd.Timestamp.now())
+results.insert(0, 'comment', comment)
 print(results)
 if not os.path.exists('results.csv'):
     results.to_csv('results.csv', index=False, mode='x',header=True)
